4b5758b39c3e9e6b8a06ad0af1c0655baa0cbf55
Jens Rehsack <sno@netbsd.org>
2017-12-19 18:20:57 +0100

    Changes: prepare for release
    Bump version to 0.428 and add note about 0.427_002 re-release as proven
    for everyone as http://matrix.cpantesters.org/?dist=List-MoreUtils-XS+0.427_002
    is completely green at 2017-12-19T18:28:16
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/List/MoreUtils/XS.pm
+++ lib/List/MoreUtils/XS.pm
@@ -7,7 +7,7 @@ use base ('Exporter');
 
 use vars qw{$VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS};
 
-$VERSION = '0.427_002';
+$VERSION = '0.428';
 
 @EXPORT    = ();
 @EXPORT_OK = qw(any all none notall one
